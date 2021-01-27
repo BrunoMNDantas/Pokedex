0ff248d06626d38698d7550b07f534c36a678505
BMNDantas <bmndantas@gmail.com>
2021-01-27 19:25:48 +0000

    Glass component implemented

---
+++ src/Pokedex/LeftPanel/Header/Glass/Glass.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import styles from './Glass.module.css'
+
+class Glass extends Component {
+    render() {
+        return (
+            <div id={styles.glass} className={this.props.spin ? styles.spin : ""}>
+                <div id={styles.reflect}> </div>
+                <img id={styles.image} src={this.props.pokemon ? this.props.pokemon.image : null}></img>
+            </div>
+        )
+    }
+}
+
+export default Glass
