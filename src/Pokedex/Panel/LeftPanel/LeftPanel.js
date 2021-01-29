0ed1730f089d5a6715f982d34af150d7f2a8059b
BMNDantas <bmndantas@gmail.com>
2021-01-29 20:30:30 +0000

    LeftPanel component implemented

---
+++ src/Pokedex/Panel/LeftPanel/LeftPanel.js
@@ -0,0 +1,28 @@
+import { Component } from 'react'
+import Header from './Header/Header'
+import Panel from './Panel/Panel'
+import Actions from './Actions/Actions'
+import styles from './LeftPanel.module.css'
+
+class LeftPanel extends Component {
+    render() {
+        return (
+            <div id={styles.container}>
+                <div id={styles.header}>
+                    <Header pokemon={this.props.pokemon} />
+                </div>
+                <div id={styles.panel}>
+                    <Panel pokemon={this.props.pokemon} />
+                </div>
+                <div id={styles.actions}>
+                    <Actions onTopClick={this.props.onTopClick}
+                        onBottomClick={this.props.onBottomClick}
+                        onLeftClick={this.props.onLeftClick}
+                        onRightClick={this.props.onRightClick} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LeftPanel
