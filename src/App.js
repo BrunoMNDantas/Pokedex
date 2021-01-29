7604a2a481a1f0d7b3c1308d0e82c4aaf18754bb
BMNDantas <bmndantas@gmail.com>
2021-01-29 20:42:15 +0000

    AppComponent implemented

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React, { Component } from 'react'
-import Table from './Table'
-import Form from './Form'
+import Pokedex from './Pokedex/Pokedex'
+
+
+const containerStyle = {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+}
+const wrapperStyle = {
+    width: "750px",
+    height: "500px"
+}
 
 class App extends Component {
-    state = {
-        characters: []
-    }
-
-    removeCharacter = (index) => {
-        const { characters } = this.state
-
-        this.setState({
-            characters: characters.filter((character, i) => {
-                return i !== index
-            }),
-        })
-    }
-
-    handleSubmit = (character) => {
-        this.setState({ characters: [...this.state.characters, character] })
-    }
-
     render() {
-        const { characters } = this.state
         return (
-            <div className="container">
-                <Table characterData={characters} removeCharacter={this.removeCharacter} />
-                <Form handleSubmit={this.handleSubmit} />
+            <div className="container" style={containerStyle}>
+                <div className="wrapper" style={wrapperStyle}>
+                    <Pokedex />
+                </div>
             </div>
         )
     }
