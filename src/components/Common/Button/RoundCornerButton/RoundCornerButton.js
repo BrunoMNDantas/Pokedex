2006d47a63fba2515d9e0cb1dd679aa35e98189c
BMNDantas <bmndantas@gmail.com>
2022-01-01 19:22:00 +0000

    Types Button added

--- src/components/Common/Button/RoundCornerButton/RoundCornerButton.js
+++ src/components/Common/Button/RoundCornerButton/RoundCornerButton.js
@@ -4,7 +4,7 @@ import Button from '../Button'
 export default function RoundCornerButton(props) {
     return (
         <div id={styles.button}>
-            <Button onClick={props.onClick} className={props.className}></Button>
+            <Button onClick={props.onClick} className={props.className}>{props.children}</Button>
         </div>
     )
 }
--- src/components/Panel/RightPanel/RightPanel.js
+++ src/components/Panel/RightPanel/RightPanel.js
@@ -1,4 +1,5 @@
 import styles from './RightPanel.module.css'
+import TypeButton from './TypeButton/TypeButton'
 import RoundCornerBlueButton from '../../Common/Button/RoundCornerButton/RoundCornerBlueButton/RoundCornerBlueButton'
 import RoundOrangeLed from '../../Common/Led/RoundLed/RoundOrangeLed/RoundOrangeLed'
 import RoundGreenLed from '../../Common/Led/RoundLed/RoundGreenLed/RoundGreenLed'
@@ -8,6 +9,7 @@ import DetailsScreen from '../../Screen/DetailsScreen/DetailsScreen'
 import BackImageMiniScreen from '../../Screen/MiniScreen/BackImageMiniScreen/BackImageMiniScreen'
 import FrontImageMiniScreen from '../../Screen/MiniScreen/FrontImageMiniScreen/FrontImageMiniScreen'
 import { useSelector } from 'react-redux'
+import { TYPES } from './TypeButton/TypeButton'
 
 export default function RightPanel(props) {
     const pokemon = useSelector(state => state.pokemon.currentPokemon) 
@@ -19,18 +21,28 @@ export default function RightPanel(props) {
             </div>
             <div id={styles.secondRow}>
                 <div id={styles.secondRow_firstRow}>
-                    <RoundCornerBlueButton />
-                    <RoundCornerBlueButton />
-                    <RoundCornerBlueButton />
-                    <RoundCornerBlueButton />
-                    <RoundCornerBlueButton />
+                    <TypeButton type={TYPES[0].name}/>
+                    <TypeButton type={TYPES[1].name}/>
+                    <TypeButton type={TYPES[2].name}/>
+                    <TypeButton type={TYPES[3].name}/>
+                    <TypeButton type={TYPES[4].name}/>
+                    <TypeButton type={TYPES[5].name}/>
                 </div>
                 <div id={styles.secondRow_secondRow}>
-                    <RoundCornerBlueButton />
-                    <RoundCornerBlueButton />
-                    <RoundCornerBlueButton />
-                    <RoundCornerBlueButton />
-                    <RoundCornerBlueButton />
+                    <TypeButton type={TYPES[6].name}/>
+                    <TypeButton type={TYPES[7].name}/>
+                    <TypeButton type={TYPES[8].name}/>
+                    <TypeButton type={TYPES[9].name}/>
+                    <TypeButton type={TYPES[10].name}/>
+                    <TypeButton type={TYPES[11].name}/>
+                </div>
+                <div id={styles.secondRow_thirdRow}>
+                    <TypeButton type={TYPES[12].name}/>
+                    <TypeButton type={TYPES[13].name}/>
+                    <TypeButton type={TYPES[14].name}/>
+                    <TypeButton type={TYPES[15].name}/>
+                    <TypeButton type={TYPES[16].name}/>
+                    <TypeButton type={TYPES[17].name}/>
                 </div>
             </div>
             <div id={styles.thirdRow}>
